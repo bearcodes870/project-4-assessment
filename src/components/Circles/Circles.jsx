e8be66142b0f57dca79b02e486b68a25d6f17c90
Bear <bear.ranasinghe@gmail.com>
2021-02-20 14:48:12 -0500

    circles render

---
+++ src/components/Circles/Circles.jsx
@@ -0,0 +1,13 @@
+import styles from './Circles.module.css';
+
+const Circles = () => (
+    <div className={styles.Circles}>
+        <div className={styles.Circles}>1</div>
+        <div className={styles.Circles}>2</div>
+        <div className={styles.Circles}>3</div>
+        <div className={styles.Circles}>4</div>
+    </div>
+  );
+  
+  export default Circles;
+  
--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import CircleSelector from '../../components/CircleSelector/CircleSelector';
+import Circles from '../../components/Circles/Circles';
 import './App.css';
 
 class App extends Component {
@@ -15,6 +16,10 @@ class App extends Component {
             selColorIdx: 1
         };
       }
+
+    handleClick = (idx) => {
+        this.setState({selColorIdx: idx});
+      }
     
 
   render() {
@@ -25,7 +30,9 @@ class App extends Component {
             Selected Color: {this.state.selColorIdx}
           <CircleSelector 
            selColorIdx = {this.state.selColorIdx}
+           handleClick = {this.handleClick}
           />
+          <Circles />
         </main>
       </div>
     );
